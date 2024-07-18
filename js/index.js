8d4bce858d730c18b89793e751435a99690b21e4
barnaste <stefanbarna10@gmail.com>
2024-07-18 10:58:18 -0400

    spinner
    added a spinner that appears while the page is loading

--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
+/* PRELOAD SPINNER EFFECT */
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+    loader.classList.add("loader-hidden");
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild("loader");
+    });
+})
+
 /* MOUSE HOVER EFFECT */
 const home = document.getElementById("home");
 const circ = document.querySelector(".circle");
 const rad = circ.clientWidth / 2;
 
 // change the position of the circle when moving around home
-home.addEventListener("mousemove", function(e) {
+home.addEventListener("mousemove", (e) => {
     circ.style.top = e.clientY - rad + "px";
     circ.style.left = e.clientX - rad + "px";
 });
@@ -16,7 +25,7 @@ const text = document.getElementById('home-text'),
     style = window.getComputedStyle(text);
 const txtop = style.getPropertyValue('top');
 
-window.addEventListener('scroll', function(e) {
+window.addEventListener('scroll', () => {
     var value = window.scrollY;
     bg.style.top = value * 0.3 + 'px';
     fg.style.top = value * 0.1 + 'px';
